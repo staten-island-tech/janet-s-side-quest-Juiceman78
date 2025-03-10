748b12248bd8508fd9b0b0d604ced1d32df7b9d6
Juiceman78 <vincentz78@nycstudents.net>
2025-03-10 10:17:10 -0400

    asda

--- app.py
+++ app.py
@@ -13,7 +13,24 @@ def csv_to_list(file_path):
 
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
-print(data)  # Output the list
+  # Output the list
+
+
+def single(data):
+    row_totals = {}
+    for row in data [1: ]: 
+        store_name = row[0]
+        sales = map(int, row[1:])
+        row_totals[store_name] = sum(sales)
+    return row_totals
+
+totals = single(data)
+print(totals)
+
+    
+
+
+
 
 
 
